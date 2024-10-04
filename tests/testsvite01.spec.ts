5afcf2385e45e070456e474a00f13ac1bd37243e
Andrey Yakovlev <vahejguru@outlook.com>
2024-10-04 19:22:17 +0200

    add before each to testsvite01 & update TC01-02

--- tests/testsvite01.spec.ts
+++ tests/testsvite01.spec.ts
@@ -1,22 +1,32 @@
 import { test, expect } from '@playwright/test';
-import { TIMEOUT } from 'dns';
+// import { TIMEOUT } from 'dns';
 
 test.describe('Front-End Tests', () => {
-  test('TC01 Login', async ({ page }) => {
+
+  test.beforeEach(async ({page}) => {
+
     await page.goto('http://localhost:3000');
     await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
     await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
-    await expect(page.getByRole('heading', {name: 'Tester Hotel Overview'})).toBeVisible;
+    await page.getByRole('button', { name: 'Login' }).click();
+  })
 
+  test('TC01 Login', async ({ page }) => {
+    // await page.goto('http://localhost:3000');
+    // await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+    // await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+    // await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByRole('heading', {name: 'Tester Hotel Overview'})).toBeVisible;
 
   });
   test('TC02 Dasboard navigation', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
-    await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+    // await page.goto('http://localhost:3000');
+    // await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+    // await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
     await expect(page.getByRole('heading', {name: 'Tester Hotel Overview'})).toBeVisible;
-  await page.locator('#app > div > div > div:nth-child(1) > a').click();
-  await expect(page).toHaveURL('http://localhost:3000/rooms');
+    // await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#app > div > div > div:nth-child(1) > a').click();
+    await expect(page).toHaveURL('http://localhost:3000/rooms');
   });
 
 });
