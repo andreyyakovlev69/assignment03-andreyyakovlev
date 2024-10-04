9bdcf20c132fece689c9ec77f5bb8f9225c95d90
Andrey Yakovlev <vahejguru@outlook.com>
2024-10-04 18:55:55 +0200

    create TC02

--- tests/testsvite01.spec.ts
+++ tests/testsvite01.spec.ts
@@ -1,12 +1,22 @@
 import { test, expect } from '@playwright/test';
+import { TIMEOUT } from 'dns';
 
 test.describe('Front-End Tests', () => {
-  test('TC01', async ({ page }) => {
+  test('TC01 Login', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
     await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
     await expect(page.getByRole('heading', {name: 'Tester Hotel Overview'})).toBeVisible;
 
+
+  });
+  test('TC02 Dasboard navigation', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+    await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+    await expect(page.getByRole('heading', {name: 'Tester Hotel Overview'})).toBeVisible;
+  await page.locator('#app > div > div > div:nth-child(1) > a').click();
+  await expect(page).toHaveURL('http://localhost:3000/rooms');
   });
 
 });
