3b1d1d05a24e884a75a35cf664c6123916727496
Andrey Yakovlev <vahejguru@outlook.com>
2024-10-07 10:01:16 +0200

    create TC03 & update beforeEach login

--- tests/testsvite01.spec.ts
+++ tests/testsvite01.spec.ts
@@ -1,33 +1,35 @@
 import { test, expect } from '@playwright/test';
-// import { TIMEOUT } from 'dns';
 
 test.describe('Front-End Tests', () => {
 
   test.beforeEach(async ({page}) => {
 
-    await page.goto('http://localhost:3000');
+    await page.goto('http://localhost:3000', {waitUntil: 'networkidle'});
     await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
     await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
     await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForURL('http://localhost:3000/');
   })
 
   test('TC01 Login', async ({ page }) => {
-    // await page.goto('http://localhost:3000');
-    // await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
-    // await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
-    // await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForURL('http://localhost:3000/', {waitUntil: 'networkidle'});
     await expect(page.getByRole('heading', {name: 'Tester Hotel Overview'})).toBeVisible;
 
   });
+
   test('TC02 Dasboard navigation', async ({ page }) => {
-    // await page.goto('http://localhost:3000');
-    // await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
-    // await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
-    await expect(page.getByRole('heading', {name: 'Tester Hotel Overview'})).toBeVisible;
-    // await page.getByRole('button', { name: 'Login' }).click();
-    await page.locator('#app > div > div > div:nth-child(1) > a').click();
+    await page.waitForURL('http://localhost:3000/', {waitUntil: 'networkidle'});
+    await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).getByRole('link').click();
     await expect(page).toHaveURL('http://localhost:3000/rooms');
     await expect(page.getByText('Rooms')).toBeVisible;
+  });
+  
+  test('TC03 New Room navigation', async ({ page }) => {
+    await page.waitForURL('http://localhost:3000/', {waitUntil: 'networkidle'});
+    await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).getByRole('link').click();
+    await page.getByRole('link', { name: 'Create Room' }).click();
+    await expect(page).toHaveURL('http://localhost:3000/room/new');
+    await expect(page.getByText('New Room')).toBeVisible;
   });
 
 });
